cc43d2c4d79c06ce7ec0594e745647561a671b3c
Suelol <kamalovramil432@gmail.com>
2024-11-29 10:07:24 +0300

    init

---
+++ FitnesClub/FitnessClub/DBModel/ConnectionClass.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FitnessClub.DBModel
+{
+    public static class ConnectionClass
+    {
+        public static FitnessClub_Kam_DavEntities fitness = new FitnessClub_Kam_DavEntities();
+    }
+}
