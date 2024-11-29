a0859e3108a2a84d21d2d2313afcc7f2f8284df4
Suelol <kamalovramil432@gmail.com>
2024-11-29 10:05:05 +0300

    init

---
+++ Fitness/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace Fitness
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
